ed060253c750632ae3442d57b5e5e66d5d8eaf3c
yoonkyoungsoo <edward4992@naver.com>
2024-05-02 01:04:31 +0900

    feat:날씨 rfc기본틀 작성 및 tsx,scss 연결 후 기본 틀 작성

--- src/pages/BugMain.tsx
+++ src/pages/BugMain.tsx
@@ -1,7 +1,25 @@
 // 해충도감 메인 페이지
 
-import React from 'react';
+import "../styles/bugmain.scss";
 
 export default function BugMain() {
-  return <div>BugMain</div>;
+  return (
+    <>
+      <body>
+        <header>헤더자리</header>
+        <div className="bug-title">병해충 도감</div>
+        <div className="line"></div>
+
+        <div className="search-container">
+          <div>병원체/작물명</div>
+          <form action="">
+            <input type="text" placeholder="검색어를 입력하세요." />
+          </form>
+        </div>
+
+        <div>API 들어갈 자리</div>
+        <footer>푸더자리</footer>
+      </body>
+    </>
+  );
 }
