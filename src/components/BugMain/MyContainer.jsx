9f58a35f772179caffb9b259317cdb5d94fa4bed
yoonkyoungsoo <edward4992@naver.com>
2024-05-08 11:48:34 +0900

    styles/ 카테고리와 검색창 위치 조정

--- src/components/BugMain/MyContainer.jsx
+++ src/components/BugMain/MyContainer.jsx
@@ -5,15 +5,6 @@ function MyContainer(props) {
   return (
     <div>
       {/* title prop 값에 따라 제목 행 렌더링 여부 결정 */}
-      {props.title && (
-        <div className="api-container-title">
-          <div className="api-item">순번</div>
-          <div className="api-item">작물</div>
-          <div className="api-item">이름</div>
-          <div className="api-item">영문영</div>
-          <div className="api-item">대표사진</div>
-        </div>
-      )}
 
       {/* 데이터 행 */}
       <div className="api-container">
--- src/pages/BugMain.jsx
+++ src/pages/BugMain.jsx
@@ -24,50 +24,53 @@ export default function BugMain() {
 
   return (
     <div>
-      <header>헤더자리</header>
       <div className="bug-title">병해충 도감</div>
       <div className="line"></div>
-      <NavbarDarkExample />
-      <div className="search-container">
-        <div>병원체/작물명</div>
-        <form className="input">
-          <input
-            type="text"
-            placeholder="검색어를 입력하세요."
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+
+      <div className="BugPageMain">
+        <div className="bootstrap-container">
+          <NavbarDarkExample />
+        </div>
+        <div className="Main-Main">
+          <div className="search-container">
+            <div>병원체/작물명</div>
+            <form className="input">
+              <input
+                type="text"
+                placeholder="검색어를 입력하세요."
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <button type="button" onClick={handleSearch}>
+                조회
+              </button>
+            </form>
+          </div>
+
+          <div className="api-container-title">
+            <div className="api-item">순번</div>
+            <div className="api-item">작물</div>
+            <div className="api-item">이름</div>
+            <div className="api-item">영문영</div>
+            <div className="api-item">대표사진</div>
+          </div>
+
+          <MyContainer
+            order="1"
+            crop="바나나"
+            name="Banana"
+            englishName="Banana"
+            mainImage="[대표사진]"
           />
-          <button type="button" onClick={handleSearch}>
-            조회
-          </button>
-        </form>
+          <MyContainer
+            order="1"
+            crop="바나나"
+            name="Banana"
+            englishName="Banana"
+            mainImage="[대표사진]"
+          />
+        </div>
       </div>
-
-      <MyContainer
-        title={true}
-        order="1"
-        crop="바나나"
-        name="Banana"
-        englishName="Banana"
-        mainImage="[대표사진]"
-      />
-      <MyContainer
-        title={false}
-        order="2"
-        crop="사과"
-        name="apple"
-        englishName="apple"
-        mainImage="[사과사진]"
-      />
-      <MyContainer
-        title={false}
-        order="3"
-        crop="회"
-        name="회 졸맛"
-        englishName="회 졸맛탱"
-        mainImage="[회먹으러가자]"
-      />
-
       <footer>푸더자리</footer>
     </div>
   );
