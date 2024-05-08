a5324d17b7962d1190af250928d80a82a5dcc1a2
hjinn0813 <hjc3790@gmail.com>
2024-05-08 19:44:11 +0900

    feat: 날씨 위젯 완료

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -15,7 +15,6 @@ export default function Weather() {
   const [nowWeather, setNowWeather] = useState('');
   const [temp, setTemp] = useState(0);
   const [error, setError] = useState('');
-  const [backgroundImg, setBackgroundImg] = useState('');
 
   useEffect(() => {
     getCurrentLocation();
@@ -36,56 +35,16 @@ export default function Weather() {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-
         setPlace(data.name);
         setIcon(data.weather[0].icon);
         setNowWeather(data.weather[0].description);
         setTemp(Math.round(data.main.temp)); // 소수점 이하 제거
-
-        // Set background image based on weather
-        setBackgroundImg(getBackgroundImg(data.weather[0].main));
       })
       .catch((error) => setError('ERROR'));
   };
 
-  const getBackgroundImg = (weatherCode) => {
-    if (weatherCode >= 200 && weatherCode < 300) {
-      // thunder storm
-      return '../../assets/weather/thunderstorm.jpg';
-    } else if (weatherCode >= 300 && weatherCode < 600) {
-      // rain
-      return '../../assets/weather/rain.jpg';
-    } else if (weatherCode >= 600 && weatherCode < 700) {
-      // snow
-      return '../../assets/weather/snow.jpg';
-    } else if (weatherCode >= 700 && weatherCode < 800) {
-      // atmosphere
-      return '../../assets/weather/atmosphere.jpg';
-    } else if (weatherCode === 800) {
-      // clear sky
-      return '../../assets/weather/clear_sky.jpg';
-    } else if (weatherCode === 801) {
-      // cloudy
-      return '../../assets/weather/cloudy.jpg';
-    } else if (weatherCode === 802) {
-      // cloudy
-      return '../../assets/weather/cloudy.jpg';
-    } else if (weatherCode === 803 || weatherCode === 804) {
-      // cloudy
-      return '../../assets/weather/cloudy.jpg';
-    } else {
-      // default -> sprout
-      return '../../assets/weather/sprout.jpg';
-    }
-  };
-
   return (
     <div className="weather-main">
-      <div
-        className="weather-background"
-        style={{ backgroundImage: `url(${backgroundImg})` }}
-      ></div>
       <div className="weather-place">{place}</div>
       {icon && (
         <img
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,22 @@
 // 전체 메인 페이지
+import React from 'react';
+import Header from '../components/Header/Header';
+import Banner from '../components/Main/Banner';
+import '../styles/Main/banner.scss';
+import Pictures from '../components/Main/picture';
+import '../styles/Main/pictures.scss';
+import MainList from '../components/Main/mainList';
+import '../styles/Main/mainList.scss';
+import Dust from '../components/Weather/Dust';
+import Weather from '../components/Weather/Weather';
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <>
+      <Banner />
+      <Weather />
+      <MainList />
+      <Pictures />
+    </>
+  );
 }
