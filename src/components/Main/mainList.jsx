3c5c707ef84a7db95e45c859b977cdb16904da95
tngml-son <sonsu6@naver.com>
2024-05-10 15:46:16 +0900

    MainPageList

--- src/components/Main/mainList.jsx
+++ src/components/Main/mainList.jsx
@@ -8,69 +8,39 @@ const Recommendation = () => {
       <div className="Recommendation">
         <h2>밭드림,</h2>
         <p>
-          밭드림은 '밭'에서 꿈을 '드림(Dream)'이라는 의미로,
+          밭드림은 '밭'에서 꿈을 <strong>'드림(Dream)'</strong> 이라는 의미로,
           <br />
           주말농장에서 자신의 꿈을 키우는 것을 의미합니다.
           <br />
-          농사 지을 밭과 꿈을 이루는 드림의 말장난 + 주말농장이 밭을 빌려주는
-          형식이므로
-          <br />
-          밭을 준다(밭드림)는 직관적인 표현 사용.
+          농사 지을 밭과 꿈을 이루는 드림의 말장난 <strong>+</strong> 주말농장이
+          밭을 <br />
+          빌려주는 형식으로 밭을 준다(밭드림)는 직관적인 표현 사용.
         </p>
       </div>
-      <div className="image-container">
-        <img src={recommendationImage} alt="추천작물" />
+      <div className="description-container">
         <div className="description">
-          <h3>추천작물</h3>
+          <h3>B</h3>
           <p>
-            주말농장에서는 다양한 작물을 재배할 수 있습니다.
-            <br /> 주변 환경에 적합한 토마토, 상추, 당근 등의 작물을 재배하는
-            것을 추천합니다.
+            <strong>B</strong>outiful harvest, fields wide
           </p>
-          <p>
-            이러한 작물들은 특별한 관리가 필요하지 않으며,
-            <br /> 주말농장에서 즐겁고 손쉽게 재배할 수 있습니다.
-            <br /> 더 나은 환경을 위해 자연친화적인 방법으로 재배할 수 있는 팁도
-            함께 제공됩니다.
-          </p>
+          <p className="Pcolor">풍부한 수확, 넓은 들판,</p>
         </div>
-        <button>바로가기</button>
-      </div>
-      <div className="image-container">
-        <img src={recommendationImage} alt="추천작물" />
+
         <div className="description">
-          <h3>추천작물</h3>
+          <h3>A</h3>
           <p>
-            주말농장에서는 다양한 작물을 재배할 수 있습니다.
-            <br /> 주변 환경에 적합한 토마토, 상추, 당근 등의 작물을 재배하는
-            것을 추천합니다.
+            <strong>A</strong>bundant joy, where dreams reside.
           </p>
-          <p>
-            이러한 작물들은 특별한 관리가 필요하지 않으며,
-            <br /> 주말농장에서 즐겁고 손쉽게 재배할 수 있습니다.
-            <br /> 더 나은 환경을 위해 자연친화적인 방법으로 재배할 수 있는 팁도
-            함께 제공됩니다.
-          </p>
+          <p className="Pcolor">풍요로운 기쁨, 꿈이 있는 곳.</p>
         </div>
-        <button>바로가기</button>
-      </div>
-      <div className="image-container">
-        <img src={recommendationImage} alt="추천작물" />
+
         <div className="description">
-          <h3>추천작물</h3>
+          <h3>T</h3>
           <p>
-            주말농장에서는 다양한 작물을 재배할 수 있습니다.
-            <br /> 주변 환경에 적합한 토마토, 상추, 당근 등의 작물을 재배하는
-            것을 추천합니다.
+            <strong>T</strong>hriving farm, nature's pride.
           </p>
-          <p>
-            이러한 작물들은 특별한 관리가 필요하지 않으며,
-            <br /> 주말농장에서 즐겁고 손쉽게 재배할 수 있습니다.
-            <br /> 더 나은 환경을 위해 자연친화적인 방법으로 재배할 수 있는 팁도
-            함께 제공됩니다.
-          </p>
+          <p className="Pcolor">번영하는 농장, 자연의 자랑.</p>
         </div>
-        <button>바로가기</button>
       </div>
     </div>
   );
