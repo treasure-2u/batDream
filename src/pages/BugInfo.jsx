4f24da97ae27e99c2568c2005f9c11113f266bd6
yoonkyoungsoo <edward4992@naver.com>
2024-05-10 18:58:59 +0900

    feat:벌레상세페이지 디자인 시작

--- src/pages/BugInfo.jsx
+++ src/pages/BugInfo.jsx
@@ -1,7 +1,20 @@
 // 해충도감 상세 페이지
 
 import React from 'react';
-
+import NavbarDarkExample from '../components/BugMain/bootstrap';
+import '../styles/BugInfo/BugInfo.scss';
 export default function BugInfo() {
-  return <div>BugInfo</div>;
+  return (
+    <>
+      <div className="bug-title">병원체</div>
+      <div className="BugInfo">
+        <div className="line"></div>
+        <div className="bootstrap-container">
+          <div>병해충 정보</div>
+          <NavbarDarkExample />
+        </div>
+        <div className="Phytophthora-page">벌레</div>
+      </div>
+    </>
+  );
 }
