7d3e394053ff79791886b976d4d2da777a398a00
yoonkyoungsoo <edward4992@naver.com>
2024-05-12 16:26:29 +0900

    feat: 상세페이지 컴포넌트 생성 및 디자인

--- src/pages/BugInfo.jsx
+++ src/pages/BugInfo.jsx
@@ -1,20 +1,15 @@
 // 해충도감 상세 페이지
 
 import React from 'react';
-import NavbarDarkExample from '../components/BugMain/bootstrap';
 import '../styles/BugInfo/BugInfo.scss';
+import InfoComponent from './../components/BugInfo/InfoComponent';
+
 export default function BugInfo() {
   return (
     <>
       <div className="bug-title">병원체</div>
-      <div className="BugInfo">
-        <div className="line"></div>
-        <div className="bootstrap-container">
-          <div>병해충 정보</div>
-          <NavbarDarkExample />
-        </div>
-        <div className="Phytophthora-page">벌레</div>
-      </div>
+      <div className="line"></div>
+      <InfoComponent />
     </>
   );
 }
