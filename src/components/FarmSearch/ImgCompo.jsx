d06f0cf5944ffcd03ec588b9c2fff33585d0bd2f
oooohri <127761560+oooohri@users.noreply.github.com>
2024-05-13 16:25:50 +0900

    fix : 이미지 오류 수정

--- src/components/FarmSearch/ImgCompo.jsx
+++ src/components/FarmSearch/ImgCompo.jsx
@@ -1,47 +1,14 @@
-import React, { useState, useEffect } from 'react';
-// import { imgArr } from '../../utils/imgarr';
-import imageUrls from '../../assets/farm/data';
-
 const ImgCompo = ({ farm }) => {
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        imageUrls.map((imageUrl, index) => {
-          console.log(`Image ${index + 1}: ${imageUrl}`);
-        });
-      } catch (error) {
-        setError(error);
-        console.error('Fetch error:', error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
   return (
     <div>
-      {imageUrls.map((imageUrl, index) => (
-        <img
-          key={index}
-          src={imageUrl}
-          alt={`Farm Image ${index + 1}`}
-          className="farm-image"
-        />
-      ))}
+      <img src={farm.imgUrl} alt={`Farm Image`} className="farm-image" />
       <div className="farmSearchContent">
-        {farm && (
-          <div className="farmSearchContent">
-            <div>
-              <p>농장명: {farm.name}</p>
-              <p>주소: {farm.address}</p>
-            </div>
+        <div className="farmSearchContent">
+          <div>
+            <p>농장명: {farm.FARM_NAME._text}</p>
+            <p>주소: {farm.ADDRESS._text}</p>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
