3f742ef8a29f5c90b72f2b3e8913c467fd0612e3
treasure-2u <dldbwls1335@naver.com>
2024-05-14 11:35:05 +0900

    delete: kakao api node module, component delete, BamilFarm component delete

--- src/pages/FarmInfo.jsx
+++ src/pages/FarmInfo.jsx
@@ -1,12 +1,10 @@
 import FarmInfoContainer from '../components/FarmInfo/FarmInfoContainer';
-// import KakaoMap from '../components/FarmInfo/KakaoMap';
 
 const App = () => {
   return (
     <>
       <h1>주말농장 현황</h1>
       <FarmInfoContainer />
-      {/* <KakaoMap /> */}
     </>
   );
 };
