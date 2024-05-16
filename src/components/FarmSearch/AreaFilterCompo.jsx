c921bc4ef31b7d4085c65110ac2f3563f69b8a2d
treasure-2u <dldbwls1335@naver.com>
2024-05-16 10:50:24 +0900

    fix:AreaFilterCompo.jsx react dom error 해결(defaultValue 추가)

--- src/components/FarmSearch/AreaFilterCompo.jsx
+++ src/components/FarmSearch/AreaFilterCompo.jsx
@@ -3,8 +3,8 @@ export default function AreaFilterCompo() {
     <div>
       <div className="filter areaFilter">
         <div className="name filterArea">지역ㅤ</div>
-        <select className="selectBox">
-          <option disabled selected>
+        <select className="selectBox" defaultValue="">
+          <option disabled value="">
             지역을 선택하세요.
           </option>
           <option value="옥길동">옥길동</option>
