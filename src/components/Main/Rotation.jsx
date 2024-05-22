d50553d2f728842522dd7d7d81737bef07f6490a
tngml-son <sonsu6@naver.com>
2024-05-22 11:39:35 +0900

    mainDesgin Rotation.jsx scssEdit

--- src/components/Main/Rotation.jsx
+++ src/components/Main/Rotation.jsx
@@ -10,35 +10,35 @@ const Rotation = () => {
       src: farmImage,
       name: '주말 농장',
       description: '주말에 농사를 지을 수 있게 농장 제공',
-      className: 'farm-image', // 각 이미지에 클래스 추가
+      className: 'rotation__image--farm',
     },
     {
       src: plantGuideImage,
       name: '식물 도감',
       description: '식물에 대한 정보를 제공',
-      className: 'plant-guide-image', // 각 이미지에 클래스 추가
+      className: 'rotation__image--plant-guide',
     },
     {
       src: pestGuideImage,
       name: '유해충 도감',
-      description: '유해충에 대한 정보를 제공 ',
-      className: 'pest-guide-image', // 각 이미지에 클래스 추가
+      description: '유해충에 대한 정보를 제공',
+      className: 'rotation__image--pest-guide',
     },
   ];
 
   return (
-    <div>
-      <div className="Introduction">Introduction</div>
-      <div className="image-container">
+    <div className="rotation">
+      <h1 className="rotation__title">Introduction</h1>
+      <div className="rotation__images">
         {images.map((image, index) => (
-          <div key={index} className="image-info">
+          <div key={index} className="rotation__image-info">
             <img
               src={image.src}
               alt={image.name}
-              className={`main-image ${image.className}`} // 클래스 추가
+              className={`rotation__image ${image.className}`}
             />
-            <p className="name">{image.name}</p>
-            <p className="description">{image.description}</p>
+            <p className="rotation__name">{image.name}</p>
+            <p className="rotation__description">{image.description}</p>
           </div>
         ))}
       </div>
